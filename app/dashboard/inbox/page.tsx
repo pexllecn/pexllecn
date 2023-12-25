a370038a48f378a6ea10edd1c339d8526b38d372
khaledlkurdi <k.alkurdi@outlook.com>
2023-12-25 01:59:47 +0000

    add inbox pages
    added inbox

--- app/dashboard/inbox/page.tsx
+++ app/dashboard/inbox/page.tsx
@@ -1,15 +1,68 @@
-import BreadCrumb from "@/components/breadcrumb";
-import { Heading } from "@/components/ui/heading";
+"use client"
+import { useEffect, useState } from 'react';
+import Image from "next/image"
 
+import { Mail } from "@/components/mail"
+import { accounts, mails } from "@/app/data"
 
-const breadcrumbItems = [{ title: "Inbox", link: "/dashboard/inbox" }];
-export default function page() {
+export default function MailPage() {
+  const [defaultLayout, setDefaultLayout] = useState(undefined);
+  const [defaultCollapsed, setDefaultCollapsed] = useState(undefined);
+
+  useEffect(() => {
+    const getCookieValue = (name) => {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(';').shift();
+      return undefined;
+    };
+
+    const layoutCookieValue = getCookieValue("react-resizable-panels:layout");
+    const collapsedCookieValue = getCookieValue("react-resizable-panels:collapsed");
+
+    console.log("layout cookie value:", layoutCookieValue);
+    console.log("collapsed cookie value:", collapsedCookieValue);
+
+    const parseCookieValue = (value) => {
+      try {
+        return value ? JSON.parse(value) : undefined;
+      } catch (error) {
+        console.error("Error parsing cookie value:", error);
+        return undefined;
+      }
+    };
+
+    setDefaultLayout(parseCookieValue(layoutCookieValue));
+    setDefaultCollapsed(parseCookieValue(collapsedCookieValue));
+  }, []);
+
   return (
     <>
-      <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
-        <BreadCrumb items={breadcrumbItems} />
-        <Heading title={`Inbox`} description="Manage tasks by Pexlle" />
+      <div className="md:hidden">
+        <Image
+          src="/examples/mail-dark.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="hidden dark:block"
+        />
+        <Image
+          src="/examples/mail-light.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="block dark:hidden"
+        />
+      </div>
+      <div className="hidden flex-col md:flex">
+        <Mail
+          accounts={accounts}
+          mails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
       </div>
     </>
-  );
+  )
 }
