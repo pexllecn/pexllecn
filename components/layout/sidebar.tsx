6d84f0b7e32497eb078494c6340c22bc8ae4277c
khaledlkurdi <k.alkurdi@outlook.com>
2023-12-26 04:03:03 +0000

    Update sidebar.tsx

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -42,7 +42,7 @@ const FooterSection = ({ theme, setTheme, isCollapsed, toggleCollapse }: { theme
     <TooltipProvider delayDuration={100}>
       <Tooltip>
         <TooltipTrigger asChild>
-          <Button variant="ghost" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+          <Button className="text-muted-foreground "variant="ghost" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
           </Button>
@@ -54,7 +54,7 @@ const FooterSection = ({ theme, setTheme, isCollapsed, toggleCollapse }: { theme
     <TooltipProvider delayDuration={100}>
   <Tooltip>
     <TooltipTrigger asChild>
-      <Button onClick={(event) => toggleCollapse()} className="p-2" variant="ghost">
+      <Button onClick={(event) => toggleCollapse()} className="p-2 text-muted-foreground" variant="ghost">
         {isCollapsed ? <PanelLeftOpen className="h-[1.2rem] w-[1.2rem]" /> : <PanelLeftClose className="h-[1.2rem] w-[1.2rem]" />}
       </Button>
     </TooltipTrigger>
