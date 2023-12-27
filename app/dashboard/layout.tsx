38e9435a900527c7018bf7fb1f0eea4e318f4605
Khaled <134864483+khaledlkurdi@users.noreply.github.com>
2023-12-27 16:28:48 +0000

    Add files via upload

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,22 +1,32 @@
-import Header from "@/components/layout/header";
+import Sidebar from "@/components/layout/sidebar";
 import type { Metadata } from "next";
-import { CenterContent } from "@/components/center";
-import { MainSidebar } from "@/components/layout/main-sidebar";
+import { MobileSidebar } from "@/components/layout/mobile-sidebar";
+import { cn } from "@/lib/utils";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Pexlle",
   description: "Basic dashboard with Next.js and Shadcn",
 };
 
-export default function DashboardLayout({}: {}) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
-      <div className="p-5 block sm:!hidden fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/50 border-b border-muted/80 bg-background/60 backdrop-blur z-20">
-        <MainSidebar />
+      <div className={cn("p-5 block sm:!hidden")}>
+        <MobileSidebar />
       </div>
       <div className="flex h-screen overflow-hidden">
-        <Header />
-        <CenterContent />
+        <Sidebar className="w-64 hidden md:block" />
+        <main className="flex-1 overflow-x-hidden overflow-y-auto ">
+          {children}
+          <Toaster richColors />
+          <SpeedInsights />
+        </main>
       </div>
     </>
   );
