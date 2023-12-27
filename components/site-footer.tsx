9e443a04bffc1c73acb1bab38315959abdc28dcd
khaledlkurdi <k.alkurdi@outlook.com>
2023-12-27 23:55:10 +0000

    Update site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -6,7 +6,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className="text-muted-foreground container flex flex-col items-center justify-between gap-4 pt-10 md:h-24 md:flex-row md:py-10">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+        <div className="flex flex-col items-center gap-4 md:flex-row md:gap-2">
           <div
             style={{
               display: "flex",
