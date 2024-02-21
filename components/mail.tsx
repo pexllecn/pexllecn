77a60d10ed1e80fdabe386cfb1e718adf2559abb
khaledlkurdi <k.alkurdi@outlook.com>
2024-02-21 22:34:10 +0000

    Update mail.tsx

--- components/mail.tsx
+++ components/mail.tsx
@@ -56,169 +56,170 @@ export function Mail({
 
   return (
     <TooltipProvider delayDuration={0}>
-      <ResizablePanelGroup
-        direction="horizontal"
-        onLayout={(sizes: number[]) => {
-          document.cookie = `react-resizable-panels:layout=${JSON.stringify(
-            sizes
-          )}`;
-        }}
-        className="h-full max-h-[860px] items-stretch"
-      >
-        <ResizablePanel
-          defaultSize={defaultLayout[0]}
-          collapsedSize={navCollapsedSize}
-          collapsible={true}
-          minSize={15}
-          maxSize={20}
-          className={cn(
-            isCollapsed && "transition-all duration-300 ease-in-out"
-          )}
+      <div className="flex flex-col h-full overflow-hidden">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="flex-grow"
+          style={{ height: "100vh" }} // Use 100vh to ensure it takes up the full viewport height
         >
-          <div className="flex items-center p-2">
-            <div
-              className="w-full"
-              // className={cn("w-full flex-1", isCollapsed ? "w-full" : "w-[90%]")}
-            >
-              <AccountSwitcher isCollapsed={isCollapsed} accounts={accounts} />
+          <ResizablePanel
+            defaultSize={defaultLayout[0]}
+            collapsedSize={navCollapsedSize}
+            collapsible={true}
+            minSize={15}
+            maxSize={20}
+            className={cn(
+              isCollapsed && "transition-all duration-300 ease-in-out"
+            )}
+          >
+            <div className="flex items-center p-2">
+              <div
+                className="w-full"
+                // className={cn("w-full flex-1", isCollapsed ? "w-full" : "w-[90%]")}
+              >
+                <AccountSwitcher
+                  isCollapsed={isCollapsed}
+                  accounts={accounts}
+                />
+              </div>
             </div>
-          </div>
-          <Separator />
-          <div className={cn(isCollapsed ? "block" : "")}>
+            <Separator />
+            <div className={cn(isCollapsed ? "block" : "")}>
+              <Nav
+                isCollapsed={isCollapsed}
+                links={[
+                  {
+                    title: "Compose",
+                    label: "",
+                    icon: PenBox,
+                    variant: "ghost",
+                  },
+                ]}
+              />
+            </div>
             <Nav
               isCollapsed={isCollapsed}
               links={[
                 {
-                  title: "Compose",
+                  title: "Notes",
+                  label: "New",
+                  icon: Inbox,
+                  variant: "default",
+                },
+                {
+                  title: "Drafts",
+                  label: "9",
+                  icon: File,
+                  variant: "ghost",
+                },
+                {
+                  title: "Shared",
                   label: "",
-                  icon: PenBox,
+                  icon: Send,
+                  variant: "ghost",
+                },
+                {
+                  title: "Junk",
+                  label: "23",
+                  icon: ArchiveX,
+                  variant: "ghost",
+                },
+                {
+                  title: "Trash",
+                  label: "",
+                  icon: Trash2,
+                  variant: "ghost",
+                },
+                {
+                  title: "Archive",
+                  label: "",
+                  icon: Archive,
                   variant: "ghost",
                 },
               ]}
             />
-          </div>
-          <Nav
-            isCollapsed={isCollapsed}
-            links={[
-              {
-                title: "Notes",
-                label: "New",
-                icon: Inbox,
-                variant: "default",
-              },
-              {
-                title: "Drafts",
-                label: "9",
-                icon: File,
-                variant: "ghost",
-              },
-              {
-                title: "Shared",
-                label: "",
-                icon: Send,
-                variant: "ghost",
-              },
-              {
-                title: "Junk",
-                label: "23",
-                icon: ArchiveX,
-                variant: "ghost",
-              },
-              {
-                title: "Trash",
-                label: "",
-                icon: Trash2,
-                variant: "ghost",
-              },
-              {
-                title: "Archive",
-                label: "",
-                icon: Archive,
-                variant: "ghost",
-              },
-            ]}
-          />
-          <Separator />
-          <Nav
-            isCollapsed={isCollapsed}
-            links={[
-              {
-                title: "Social",
-                label: "972",
-                icon: Users2,
-                variant: "ghost",
-              },
-              {
-                title: "Updates",
-                label: "342",
-                icon: AlertCircle,
-                variant: "ghost",
-              },
-              {
-                title: "Forums",
-                label: "128",
-                icon: MessagesSquare,
-                variant: "ghost",
-              },
-              {
-                title: "Shopping",
-                label: "8",
-                icon: ShoppingCart,
-                variant: "ghost",
-              },
-              {
-                title: "Promotions",
-                label: "21",
-                icon: Archive,
-                variant: "ghost",
-              },
-            ]}
-          />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={defaultLayout[1]} minSize={20}>
-          <Tabs defaultValue="all">
-            <div className="flex items-center px-4 py-2">
-              <h1 className="text-xl font-bold">Notes</h1>
-              <TabsList className="ml-auto">
-                <TabsTrigger
-                  value="all"
-                  className="text-zinc-600 dark:text-zinc-200"
-                >
-                  All Notes
-                </TabsTrigger>
-                <TabsTrigger
-                  value="unread"
-                  className="text-zinc-600 dark:text-zinc-200"
-                >
-                  Unread
-                </TabsTrigger>
-              </TabsList>
-            </div>
             <Separator />
-            <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-              <form>
-                <div className="relative">
-                  <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                  <Input placeholder="Search" className="pl-8" />
-                </div>
-              </form>
-            </div>
-            <TabsContent value="all" className="m-0">
-              <MailList items={mails} />
-            </TabsContent>
-            <TabsContent value="unread" className="m-0">
-              <MailList items={mails.filter((item) => !item.read)} />
-            </TabsContent>
-          </Tabs>
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={defaultLayout[2]}>
-          <MailDisplay
-            mail={mails.find((item) => item.id === mail.selected) || null}
-          />
-        </ResizablePanel>
-      </ResizablePanelGroup>
+            <Nav
+              isCollapsed={isCollapsed}
+              links={[
+                {
+                  title: "Social",
+                  label: "972",
+                  icon: Users2,
+                  variant: "ghost",
+                },
+                {
+                  title: "Updates",
+                  label: "342",
+                  icon: AlertCircle,
+                  variant: "ghost",
+                },
+                {
+                  title: "Forums",
+                  label: "128",
+                  icon: MessagesSquare,
+                  variant: "ghost",
+                },
+                {
+                  title: "Shopping",
+                  label: "8",
+                  icon: ShoppingCart,
+                  variant: "ghost",
+                },
+                {
+                  title: "Promotions",
+                  label: "21",
+                  icon: Archive,
+                  variant: "ghost",
+                },
+              ]}
+            />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={defaultLayout[1]} minSize={20}>
+            <Tabs defaultValue="all">
+              <div className="flex items-center px-4 py-2">
+                <h1 className="text-xl font-bold">Notes</h1>
+                <TabsList className="ml-auto">
+                  <TabsTrigger
+                    value="all"
+                    className="text-zinc-600 dark:text-zinc-200"
+                  >
+                    All Notes
+                  </TabsTrigger>
+                  <TabsTrigger
+                    value="unread"
+                    className="text-zinc-600 dark:text-zinc-200"
+                  >
+                    Unread
+                  </TabsTrigger>
+                </TabsList>
+              </div>
+              <Separator />
+              <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+                <form>
+                  <div className="relative">
+                    <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                    <Input placeholder="Search" className="pl-8" />
+                  </div>
+                </form>
+              </div>
+              <TabsContent value="all" className="m-0">
+                <MailList items={mails} />
+              </TabsContent>
+              <TabsContent value="unread" className="m-0">
+                <MailList items={mails.filter((item) => !item.read)} />
+              </TabsContent>
+            </Tabs>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={defaultLayout[2]}>
+            <MailDisplay
+              mail={mails.find((item) => item.id === mail.selected) || null}
+            />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </TooltipProvider>
   );
 }
