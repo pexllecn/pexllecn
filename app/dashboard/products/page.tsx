0456affa57d2192cb459f34c4d18a5c036bce982
khaledlkurdi <k.alkurdi@outlook.com>
2024-03-08 16:16:04 +0000

    Update page.tsx

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,12 +1,25 @@
+"use client";
 import BreadCrumb from "@/components/breadcrumb";
 import { Heading } from "@/components/ui/heading";
+import { motion } from "framer-motion";
 
 const breadcrumbItems = [{ title: "Products", link: "/dashboard/products" }];
 export default function page() {
+  const variants1 = {
+    hidden: { filter: "blur(10px)", opacity: 0 },
+    visible: { filter: "blur(0px)", opacity: 1 },
+  };
   return (
-    <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
-      <BreadCrumb items={breadcrumbItems} />
-      <Heading title={`Products`} description="" />{" "}
-    </div>
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 0.3 }}
+      variants={variants1}
+    >
+      <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
+        <BreadCrumb items={breadcrumbItems} />
+        <Heading title={`Products`} description="" />{" "}
+      </div>
+    </motion.div>
   );
 }
