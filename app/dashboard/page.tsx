785bcc6282d7def6919a967669f8d55dddc0e5df
khaledlkurdi <k.alkurdi@outlook.com>
2024-03-08 12:59:41 +0000

    add dynamic change in particles

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,12 +2,20 @@
 import React from "react";
 import { SparklesCore } from "@/components/ui/sparkles";
 import { motion } from "framer-motion";
+import { useTheme } from "next-themes";
 
 export default function SparklesPreview() {
+  const { theme = "light", setTheme } = useTheme(); // Default to 'light' if theme is undefined
+
+  // Determine particle color based on theme
+  const particleColor = theme === "light" ? "#000000" : "#FFFFFF";
+
+  // Variants for motion.h1, assuming you've defined them elsewhere
   const variants1 = {
-    hidden: { filter: "blur(10px)", opacity: 0 },
-    visible: { filter: "blur(0px)", opacity: 1 },
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
   };
+
   return (
     <div className="relative h-full w-full flex flex-col items-center justify-center overflow-hidden">
       <div className="w-full absolute inset-0 h-screen">
@@ -18,7 +26,7 @@ export default function SparklesPreview() {
           maxSize={1.4}
           particleDensity={100}
           className="w-full h-full"
-          particleColor="#808080"
+          particleColor={particleColor} // Dynamically set based on theme
         />
       </div>
       <motion.h1
