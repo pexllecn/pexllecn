d51e18bea5d03068437f003aa4d9bd5e19c79763
khaledlkurdi <k.alkurdi@outlook.com>
2024-03-08 18:49:36 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-"use client";
 import Header from "@/components/layout/header";
 import type { Metadata } from "next";
 import { CenterContent } from "@/components/center";
 import { MainSidebar } from "@/components/layout/main-sidebar";
 import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
 
 export const metadata: Metadata = {
   title: "Pexlle",
@@ -12,21 +10,12 @@ export const metadata: Metadata = {
 };
 
 export default function DashboardLayout({}: {}) {
-  const variants1 = {
-    hidden: { filter: "blur(10px)", opacity: 0 },
-    visible: { filter: "blur(0px)", opacity: 1 },
-  };
   return (
-    <motion.div
-      initial="hidden"
-      animate="visible"
-      transition={{ duration: 0.25 }}
-      variants={variants1}
-    >
+    <>
       <div className="flex h-screen overflow-hidden">
         <Header />
         <CenterContent />
       </div>
-    </motion.div>
+    </>
   );
 }
